4db79c47bb5e601ebcb6d2c1829640acbc9a58c2
Yuya Miyazaki <ymiyazaki@clustium.com>
2015-07-27 01:56:35 -0700

    remove debug message

--- wires.js
+++ wires.js
@@ -156,7 +156,6 @@ else {
 			var queue = new createjs.LoadQueue();
 			var bitmap = new createjs.Bitmap(image);
 			queue.addEventListener("fileload", function fileload(e) {
-				console.log("width", bitmap.image.width);
 				if (! bitmap.image.width || bitmap.image.width == 4294967295) { // fix for firefox
 					setTimeout(fileload, 100);
 					console.log('waiting...');
